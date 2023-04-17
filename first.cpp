ca6534a96214d57d14587ba22e0b94d556587e5c
Kostya Illipurov <Illipurov2134@gmail.com>
2023-04-17 20:54:01 +0400

    ДОбавил фибоначи

--- first.cpp
+++ first.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <bits/stdc++.h> 
 using namespace std;
+int f(int n) { 
+    int a, b, sum, count; 
+    a = count = sum = 0; 
+    b = 1; 
+    while (count < n) { 
+        sum = a + b; 
+        a = b; 
+        b = sum; 
+        count++; 
+    } 
+    return a; 
+} 
 int main(){
-    cout << "Hello, World!";
+    cout << "Hello, World!\n";
+    int n; 
+    cout << "n="; cin >> n; 
+    cout << f(n) << "\n"; 
     return 0;
 }
+
